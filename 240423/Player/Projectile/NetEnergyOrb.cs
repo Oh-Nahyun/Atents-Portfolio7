8ded42b846877a61db19b9febaf62ff5b9b6fcaf
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-04-24 11:18:38 +0900

    Add files via upload

---
+++ 240423/Player/Projectile/NetEnergyOrb.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class NetEnergyOrb : MonoBehaviour
+{
+    /// <summary>
+    /// 발사 초기 속도
+    /// </summary>
+    public float speed = 10.0f;
+
+    /// <summary>
+    /// 수명
+    /// </summary>
+    public float lifeTime = 20.0f;
+
+    /// <summary>
+    /// 폭발 범위
+    /// </summary>
+    public float explosionRadius = 5.0f;
+
+    Rigidbody rigid;
+    VisualEffect effect;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        effect = GetComponent<VisualEffect>();
+    }
+
+    private void Start()
+    {
+        transform.Rotate(-30.0f, 0.0f, 0.0f);
+        rigid.velocity = speed * transform.forward;
+        Destroy(this.gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider[] result = Physics.OverlapSphere(transform.position, explosionRadius, LayerMask.GetMask("Player"));
+
+        if (result.Length > 0)
+        {
+            foreach (Collider col in result)
+            {
+                Debug.Log(col.gameObject.name);
+            }
+        }
+
+        StartCoroutine(EffectFinishProcess());
+    }
+
+    IEnumerator EffectFinishProcess()
+    {
+        yield return null;
+    }
+}
+
+/// 실습_240423
+/// EffectFinishProcess() 구현하기
+/// ----------------------------------
+/// 0.5초 동안 explosionRadius까지 확대
+/// 1초 동안 baseSize가 0이 될 때까지 축소
+/// 파티클 생성 중지
+/// 파티클 개수가 0이 되면 게임 오브젝트 제거
+/// ----------------------------------
+/// effect.SetFloat("BaseSize", radius);    // 변수 변경하기 (타입에 맞게 해야함)
+/// effect.SendEvent("OnEffectFinish");     // 이벤트 날리기
+/// effect.aliveParticleCount;              // 남아있는 파티클 개수
+
+/*
+float timer;
+private void Update()
+{
+    timer = Time.deltaTime;
+}
+
+IEnumerator EffectFinishProcess()
+{
+    float radius = 0.0f;
+    int baseSize = Shader.PropertyToID("BaseSize");
+
+    timer = 0.0f;
+    while (timer < 0.5f)
+    {
+        radius = explosionRadius * timer * 2;
+        effect.SetFloat(baseSize, radius);
+    }
+
+    timer = 0.0f;
+    while (timer < 1.0f)
+    {
+        radius -= explosionRadius * timer;
+        effect.SetFloat(baseSize, radius);
+    }
+
+    effect.SendEvent("OnEffectFinish");
+    if (effect.aliveParticleCount == 0)
+    {
+        Destroy(this.gameObject);
+    }
+
+    yield return null;
+}
+ */
---
+++ 240423/UI/Test_NetController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_NetController : MonoBehaviour
+{
+    TextMeshProUGUI playerInGame;
+    TextMeshProUGUI userName;
+
+    const string BlankUserName = "********";
+    const string BlankPlayersInGame = "-";
+
+    private void Start()
+    {
+        Transform child = transform.GetChild(0);
+        Button startHost = child.GetComponent<Button>();
+        startHost.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartHost())       // 호스트로 시작 시도
+            {
+                Debug.Log("호스트로 시작 성공");
+            }
+            else
+            {
+                Debug.Log("호스트로 시작 실패");
+            }
+        });
+
+        child = transform.GetChild(1);
+        Button startClient = child.GetComponent<Button>();
+        startClient.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartClient())     // 클라이언트로 연결 시도
+            {
+                Debug.Log("클라이언트로 연결 성공");
+            }
+            else
+            {
+                Debug.Log("클라이언트로 연결 실패");
+            }
+        });
+
+        child = transform.GetChild(2);
+        Button disconnect = child.GetComponent<Button>();
+        disconnect.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.Shutdown();            // 내 연결 끊기
+        });
+
+        // 동접자 수
+        child = transform.GetChild(3);
+        child = child.GetChild(1);
+        playerInGame = child.GetComponent<TextMeshProUGUI>();
+
+        GameManager gameManager = GameManager.Instance;
+        gameManager.onPlayersInGameChange += (count) => playerInGame.text = count.ToString(); // 동접자 숫자 변경 델리게이트가 실행되면 UI 갱신
+
+        // 사용자 이름
+        child = transform.GetChild(4);
+        child = child.GetChild(1);
+        userName = child.GetComponent<TextMeshProUGUI>();
+        gameManager.onUserNameChange += (name) =>
+        {
+            userName.text = gameManager.UserName;
+        };
+
+        // 플레이어가 접속을 끊었을 때 초기화
+        gameManager.onPlayerDisconnected += () =>
+        {
+            userName.text = BlankUserName;
+            playerInGame.text = BlankPlayersInGame;
+        };
+    }
+}
